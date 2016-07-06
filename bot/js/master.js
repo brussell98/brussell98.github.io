4f7da7a9382038acdfb1f5d2eddbfcdde4a477c7
Brandon Russell <russellbrandon1999@gmail.com>
2016-07-06 16:19:21 -0500

    commands.html

--- bot/js/master.js
+++ bot/js/master.js
@@ -6,4 +6,11 @@ document.addEventListener('DOMContentLoaded', function() {
 		navMenu.classList.toggle('is-active');
 		navToggle.classList.toggle('is-active');
 	});
+
+	var cardToggles = document.getElementsByClassName('card-toggle');
+	for (var i = 0; i < cardToggles.length; i++) {
+		cardToggles[i].addEventListener('click', function(e) {
+			e.currentTarget.parentElement.parentElement.childNodes[3].classList.toggle('is-hidden');
+		});
+	}
 });
